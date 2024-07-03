509a19e342c3e37198489e69258d576bfe1b4e9b
Eleven <idiotalex@163.com>
2024-07-03 15:59:22 +0800

    chore: add unplugin-icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts">
-import ToggleDark from '@/components/ToggleDark.vue'
 import { useAppStore } from '@/stores/app.ts'
+import x from '~icons/mdi/home?raw'
 
 const appStore = useAppStore()
+const myCanvas = ref()
+
+onMounted(async () => {
+  console.log(typeof x)
+  const canvas = myCanvas.value
+  const ctx = canvas.getContext('2d')
+
+  // 获取图标的 SVG 内容
+  const svgContent = x ;//`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8h5z"/></svg>`
+  console.log({svgContent})
+  // 将 SVG 内容转换为 Blob
+  const blob = new Blob([svgContent], { type: 'image/svg+xml' })
+  // 创建 Blob URL
+  const url = URL.createObjectURL(blob)
+  // 创建 Image 对象
+  const img = new Image()
+  img.src = url
+  img.onload = () => {
+    ctx.drawImage(img, 100, 50)
+  }
+})
 </script>
 
 <template>
@@ -28,5 +49,9 @@ const appStore = useAppStore()
         </button>
       </div>
     </div>
+    <i-mdi-account />
+    <canvas ref="myCanvas" class="artboard artboard-demo">
+    </canvas>
+
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,40 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          enabledCollections: [
+            'mdi',
+          ],
+        }),
+      ],
+      dts: './components.d.ts',
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+      ],
+      dts: './auto-imports.d.ts',
+    }),
+    Icons(),
   ],
   resolve: {
     alias: {
