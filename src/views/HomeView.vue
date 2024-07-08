2cbf31ff0f1d347becb3635f28b73f6e99cadd18
Eleven <idiotalex@163.com>
2024-07-09 00:40:21 +0800

    fix: ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,13 +9,15 @@ import {
 import { useAppStore } from '@/stores/app.ts'
 import HomeSvgContent from '~icons/mdi/home?raw'
 
+
 const appStore = useAppStore()
 const myCanvas = ref(null)
 
 onMounted(async () => {
   const unescapedData = HomeSvgContent.replace('<svg', '<svg xmlns="http://www.w3.org/2000/svg"')
 
-  const fabricCanvas = new Canvas(myCanvas.value, {width: 600, height: 400});
+  const canvasWrap: any = document.getElementById('canvas-wrap')
+  const fabricCanvas = new Canvas(myCanvas.value, {width: canvasWrap.clientWidth, height: canvasWrap.clientHeight})
   const text = new Text('fabric.js', { fill: 'blue', fontSize: 24 });
   const rect = new Rect({
     top: 100,
@@ -40,8 +42,16 @@ onMounted(async () => {
 <template>
   <main :data-theme="appStore.theme" class="theme-dark bg-line" h-100vh>
     <Header />
-    <div class="artboard artboard-demo">
-      <canvas ref="myCanvas"></canvas>
+    <div flex>
+      <div p-4>
+        <div w-300px flex>
+          <i-mdi-home />
+          <i-mdi-home-account />
+        </div>
+      </div>
+      <div id="canvas-wrap" class="artboard artboard-bg">
+        <canvas ref="myCanvas"></canvas>
+      </div>
     </div>
   </main>
 </template>
